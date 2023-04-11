c9a625c1bb3471d85d0ad6f4811a138bd4863e99
정은성 <80687043+CoderTrex@users.noreply.github.com>
2023-04-11 17:19:38 +0900

    파일 위치 수정

---
+++ Game_Graphics_Programming/GCP_Texture/source/Library/Game/PS.hlsl
@@ -0,0 +1,17 @@
+#include "shader.fx"
+
+
+
+
+
+
+
+//struct VS_OUTPUT{
+//	float4 Pos : SV_POSITION;
+//	float2 TexCoord : TEXCOORD0;
+//};
+//
+//float4 PS(VS_OUTPUT input) : SV_TARGET
+//{x
+//	return input.Color;
+//}
